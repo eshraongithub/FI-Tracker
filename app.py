9ecded12e174d5c54e4e65244da289e52f8405ef
Mohamed Eshra <eshra14@hotmail.com>
2022-08-06 23:21:23 +0200

    Change name of application to app

--- app.py
+++ app.py
@@ -20,8 +20,6 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Ensure responses aren't cached
-
-
 @app.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
